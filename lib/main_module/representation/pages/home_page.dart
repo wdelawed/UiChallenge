7e5c21c29cadd7bed76dc6597d564e72f2ac2c9b
wdelawed <wdelawed@gmail.com>
2022-07-02 23:09:05 +0300

    constrained the maximum width to 570px

--- lib/main_module/representation/pages/home_page.dart
+++ lib/main_module/representation/pages/home_page.dart
@@ -62,122 +62,130 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(top: 47),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-              ),
-              child: Column(
-                children: [
-                  const CoachInfoWidget(),
-                  Container(
-                    margin: const EdgeInsets.only(top: 26),
-                    child: const Center(
-                      child: CoachBanner(),
-                    ),
+        child: Center(
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: 574),
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 47),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 8),
-                    child: DefaultTabController(
-                      length: 2,
-                      child: Column(
-                        children: [
-                          TabBar(
-                              controller: _tabController,
-                              onTap: (tab) {
-                                setState(() {
-                                  _selectedTab = tab;
-                                });
-                              },
-                              indicatorColor: kprimaryBtnBackground,
-                              labelColor: kprimaryBtnBackground,
-                              unselectedLabelColor: const Color(0xffA1A1A1),
-                              labelStyle: k12regular,
-                              tabs: [
-                                Container(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 16),
-                                  alignment: Alignment.bottomCenter,
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      SvgPicture.asset(
-                                        "assets/svgs/ic_gallery.svg",
-                                        width: 14,
-                                        height: 14,
-                                        color: _selectedTab == 0
-                                            ? kprimaryBtnBackground
-                                            : const Color(0xffA1A1A1),
-                                      ),
-                                      Container(
-                                        margin: const EdgeInsets.symmetric(
-                                            horizontal: 7),
-                                        child: const Text(
-                                          "المعرض",
-                                          style: k14regular,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Container(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 16),
-                                  alignment: Alignment.bottomCenter,
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      SvgPicture.asset(
-                                        "assets/svgs/ic_questions.svg",
-                                        width: 14,
-                                        height: 14,
-                                        color: _selectedTab == 1
-                                            ? kprimaryBtnBackground
-                                            : const Color(0xffA1A1A1),
-                                      ),
-                                      Container(
-                                        margin: const EdgeInsets.symmetric(
-                                            horizontal: 7),
-                                        child: const Text(
-                                          "شروط المدرب",
-                                          style: k14regular,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ]),
-                          SizedBox(
-                            height: 351,
-                            child: TabBarView(
-                              controller: _tabController,
-                              children: const [
-                                CoachGalleryTab(),
-                                QuestionsTabWidget(),
-                              ],
-                            ),
-                          ),
-                        ],
+                  child: Column(
+                    children: [
+                      const CoachInfoWidget(),
+                      Container(
+                        margin: const EdgeInsets.only(top: 26),
+                        child: const Center(
+                          child: CoachBanner(),
+                        ),
                       ),
-                    ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 8),
+                        child: DefaultTabController(
+                          length: 2,
+                          child: Column(
+                            children: [
+                              TabBar(
+                                  controller: _tabController,
+                                  onTap: (tab) {
+                                    setState(() {
+                                      _selectedTab = tab;
+                                    });
+                                  },
+                                  indicatorColor: kprimaryBtnBackground,
+                                  labelColor: kprimaryBtnBackground,
+                                  unselectedLabelColor: const Color(0xffA1A1A1),
+                                  labelStyle: k12regular,
+                                  tabs: [
+                                    Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 16),
+                                      alignment: Alignment.bottomCenter,
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          SvgPicture.asset(
+                                            "assets/svgs/ic_gallery.svg",
+                                            width: 14,
+                                            height: 14,
+                                            color: _selectedTab == 0
+                                                ? kprimaryBtnBackground
+                                                : const Color(0xffA1A1A1),
+                                          ),
+                                          Container(
+                                            margin: const EdgeInsets.symmetric(
+                                                horizontal: 7),
+                                            child: const Text(
+                                              "المعرض",
+                                              style: k14regular,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 16),
+                                      alignment: Alignment.bottomCenter,
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          SvgPicture.asset(
+                                            "assets/svgs/ic_questions.svg",
+                                            width: 14,
+                                            height: 14,
+                                            color: _selectedTab == 1
+                                                ? kprimaryBtnBackground
+                                                : const Color(0xffA1A1A1),
+                                          ),
+                                          Container(
+                                            margin: const EdgeInsets.symmetric(
+                                                horizontal: 7),
+                                            child: const Text(
+                                              "شروط المدرب",
+                                              style: k14regular,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ]),
+                              SizedBox(
+                                height: 351,
+                                child: TabBarView(
+                                  controller: _tabController,
+                                  children: const [
+                                    CoachGalleryTab(),
+                                    QuestionsTabWidget(),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                const CoachCustomerReviews(),
+                SubscriptionPlan(selectedPlan: _selectedPlan),
+              ],
             ),
-            const CoachCustomerReviews(),
-            SubscriptionPlan(selectedPlan: _selectedPlan),
-          ],
+          ),
         ),
       ),
       bottomNavigationBar: Container(
         height: 99,
+        constraints: const BoxConstraints(minWidth: 570),
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24.5),
         color: Colors.white,
         child: TextButton(
           style: TextButton.styleFrom(
             backgroundColor: kSecondaryBtnBackground,
+            // maximumSize: const Size(570, 50),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(8.0),
             ),
